b9e0abe074cc13c8bc01cc51dc5120e969d046ae
NachoGuidali <nachog.akd@hotmail.com>
2024-06-05 23:12:01 -0300

    Finish

--- web/settings.py
+++ web/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "webapp/staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+

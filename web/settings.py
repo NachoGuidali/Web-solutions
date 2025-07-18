98632102cb7aa9d7b0fd32dfc6b469ceae1e8d63
NachoGuidali <nachog.akd@hotmail.com>
2025-07-18 12:52:28 -0300

    debug

--- web/settings.py
+++ web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f$jo19ev_mhiew^nfumeule1ojsy+80#+pio0get=5!5wwh)10'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nachogakd.pythonanywhere.com', '127.0.0.1', 'supregsolutions.com', 'www.supregsolutions.com']
 
